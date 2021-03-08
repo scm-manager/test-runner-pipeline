4d82a69eef124f25407a7e041499e411a0c54d02
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-03-08 15:05:45 +0100

    Skip test archive

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -8,6 +8,6 @@ git clone https://github.com/scm-manager/integration-test-runner
 cd integration-test-runner
 yarn install
 ls -l
-yarn integration-test-runner collect -c
+yarn integration-test-runner collect -c -s
 yarn integration-test-runner provision -a http://scm-server:8080/scm -u scmadmin -p scmadmin
 yarn integration-test-runner run -a http://scm-server:8080/scm -u scmadmin -p scmadmin
